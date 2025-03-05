c69209dd1d5e2a523e0dc1070e7d171960975356
antalpeti <antalpeti@users.noreply.github.com>
2025-03-06 00:58:32 +0100

    refactor Demo class to use var for variable declarations and add extra greeting message

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -4,12 +4,14 @@ public class Demo {
 
   @SuppressWarnings("all")
   public static void main(String[] args) {
-    String message1 = "Hello, World!";
-    String message2 = "Hello, World of Java!";
-    String message3 = "Hello, World Again!";
+    var message1 = "Hello, World!";
+    var message2 = "Hello, World of Java!";
+    var message3 = "Hello, World Again!";
     System.out.println(message1);
     System.out.println(message2);
     System.out.println(message3);
+    var extra = "I love to code!";
+    System.out.println(message1 + " " + extra);
   }
 
 }
