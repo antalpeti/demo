95a3d2a40249dd538f8906a77da9f22d0e7eb9ea
antalpeti <antalpeti@users.noreply.github.com>
2025-03-05 23:13:34 +0100

    update Demo class to add additional greeting message

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -1,10 +1,11 @@
 package demo;
 
-@SuppressWarnings("all")
 public class Demo {
 
+  @SuppressWarnings("all")
   public static void main(String[] args) {
     System.out.println("Hello, World!");
+    System.out.println("Hello, World of Java!");
     System.out.println("Hello, World Again!");
   }
 
