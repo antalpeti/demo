875991c7cef69a006505bb90455a5ff0a4e10c49
antalpeti <antalpeti@users.noreply.github.com>
2025-03-06 02:05:03 +0100

    add StringDemo class to demonstrate string usage and output

---
+++ src/main/java/demo/StringDemo.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class StringDemo {
+
+  @SuppressWarnings({"java:S106", "java:S125"})
+  public static void main(String[] args) {
+
+    /*
+    var message = "Hello World!";
+    var extra = "I love to code!";
+     */
+
+    String message = "Hello World!";
+    String extra = "I love to code!";
+
+    System.out.println(message + " " + extra);
+  }
+
+}
