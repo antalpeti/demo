15487053ccc02b72ea2766b1693d661d43ee32e8
antalpeti <antalpeti@users.noreply.github.com>
2025-03-06 01:21:39 +0100

    update Demo class to improve greeting messages and add string manipulation outputs

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -4,14 +4,18 @@ public class Demo {
 
   @SuppressWarnings("all")
   public static void main(String[] args) {
-    var message1 = "Hello, World!";
-    var message2 = "Hello, World of Java!";
-    var message3 = "Hello, World Again!";
+    var message1 = "Hello World!";
+    var message2 = "Hello World of Java!";
+    var message3 = "Hello World Again!";
     System.out.println(message1);
     System.out.println(message2);
     System.out.println(message3);
     var extra = "I love to code!";
     System.out.println(message1 + " " + extra);
+
+    System.out.println("Length of " + message1 + " is " + message1.length());
+    System.out.println("Upper case version of " + message1 + " is " + message1.toUpperCase());
+    System.out.println("Lower case version of " + message1 + " is " + message1.toLowerCase());
   }
 
 }
