eabdd9dc8810b2ef1efe9e894ec3176907aab24b
antalpeti <antalpeti@users.noreply.github.com>
2025-03-21 08:08:46 +0100

    fix typo in SwitchComputerType class for tablet description

--- src/main/java/demo/SwitchComputerType.java
+++ src/main/java/demo/SwitchComputerType.java
@@ -16,7 +16,7 @@ public class SwitchComputerType {
         System.out.println("Smartphones offer computing power in your hand.");
         break;
       case "tablet":
-        System.out.println("Tablets are light-weight for browsing and light tasks.");
+        System.out.println("Tablets are lightweight for browsing and light tasks.");
         break;
       case "laptop":
         System.out.println("Laptops are portable for work on the go.");
