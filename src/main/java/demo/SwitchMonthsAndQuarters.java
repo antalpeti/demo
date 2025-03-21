70b07df39b52341e8c50702145c4fca67382c1dc
antalpeti <antalpeti@users.noreply.github.com>
2025-03-21 07:50:53 +0100

    add SwitchMonthsAndQuarters class to demonstrate month-to-quarter mapping using switch statements

---
+++ src/main/java/demo/SwitchMonthsAndQuarters.java
@@ -0,0 +1,43 @@
+package demo;
+
+import java.util.Scanner;
+
+public class SwitchMonthsAndQuarters {
+
+  @SuppressWarnings({"java:S106", "java:S6208"})
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter month number (1-12): ");
+    int theMonth = scanner.nextInt();
+
+    System.out.println("Month: " + theMonth);
+
+    switch (theMonth) {
+      case 1:
+      case 2:
+      case 3:
+        System.out.println("Q1");
+        break;
+      case 4:
+      case 5:
+      case 6:
+        System.out.println("Q2");
+        break;
+      case 7:
+      case 8:
+      case 9:
+        System.out.println("Q3");
+        break;
+      case 10:
+      case 11:
+      case 12:
+        System.out.println("Q4");
+        break;
+      default:
+        System.out.println("Invalid month.");
+    }
+
+    scanner.close();
+  }
+}
