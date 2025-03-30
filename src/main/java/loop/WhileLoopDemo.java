806fab6c4469e29b3d1d4dde17cce89fa6fd2f7f
antalpeti <antalpeti@users.noreply.github.com>
2025-03-30 13:29:44 +0200

    add do-while loop implementation to WhileLoopDemo for user input handling

--- src/main/java/loop/WhileLoopDemo.java
+++ src/main/java/loop/WhileLoopDemo.java
@@ -10,6 +10,27 @@ public class WhileLoopDemo {
 
     boolean done = false;
 
+//    whileLoop(done, scanner);
+    doWhile(scanner, done);
+
+    scanner.close();
+  }
+
+  private static void doWhile(Scanner scanner, boolean done) {
+    do {
+      System.out.println("Hello World!");
+
+      System.out.println("Are we done? (Y/N)");
+      String userInput = scanner.nextLine();
+
+      if (userInput.equalsIgnoreCase("Y")) {
+        done = true;
+      }
+      System.out.println();
+    } while (!done);
+  }
+
+  private static void whileLoop(boolean done, Scanner scanner) {
     while (!done) {
       System.out.println("Hello World!");
 
@@ -21,8 +42,6 @@ public class WhileLoopDemo {
       }
       System.out.println();
     }
-
-    scanner.close();
   }
 
 }
