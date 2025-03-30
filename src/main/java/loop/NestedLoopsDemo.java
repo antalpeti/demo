23d68da55d5a87034f23c5444f62b08d57180819
antalpeti <antalpeti@users.noreply.github.com>
2025-03-30 12:11:18 +0200

    update NestedLoopsDemo to generate a multiplication table for numbers 1 to 10

--- src/main/java/loop/NestedLoopsDemo.java
+++ src/main/java/loop/NestedLoopsDemo.java
@@ -4,10 +4,12 @@ public class NestedLoopsDemo {
 
   @SuppressWarnings("java:S106")
   public static void main(String[] args) {
-    //5x5 multiplication table
-    for (int row = 1; row <= 5; row++) {
-      for (int column = 1; column <= 5; column++) {
-        System.out.print(row * column + "\t");
+    for (int number = 1; number <= 10; number++) {
+      for (int row = 1; row <= number; row++) {
+        for (int column = 1; column <= number; column++) {
+          System.out.print(row * column + "\t");
+        }
+        System.out.println();
       }
       System.out.println();
     }
