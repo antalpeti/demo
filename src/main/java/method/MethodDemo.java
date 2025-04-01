2c0c8a8ae2a54ed7cb962acb34e8fe6236803f6d
antalpeti <antalpeti@users.noreply.github.com>
2025-04-01 08:34:26 +0200

    add displayManyGreetings method to print multiple greeting messages

--- src/main/java/method/MethodDemo.java
+++ src/main/java/method/MethodDemo.java
@@ -2,8 +2,22 @@ package method;
 
 public class MethodDemo {
 
+  @SuppressWarnings("java:S125")
   public static void main(String[] args) {
-    displayGreetings();
+//    displayGreetings();
+
+    displayManyGreetings(3);
+  }
+
+  @SuppressWarnings("java:S106")
+  static void displayManyGreetings(int count) {
+    for (int i = 0; i < count; i++) {
+      System.out.println("Hello World!");
+      System.out.println("Welcome, welcome!");
+      System.out.println("Please make yourself at home.");
+
+      System.out.println();
+    }
   }
 
   @SuppressWarnings("java:S106")
