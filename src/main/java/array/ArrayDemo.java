af1e47654f950ee60878e9a77573046f938a6816
antalpeti <antalpeti@users.noreply.github.com>
2025-04-03 23:21:07 +0200

    add ArrayDemo class to demonstrate array usage and properties

---
+++ src/main/java/array/ArrayDemo.java
@@ -0,0 +1,21 @@
+package array;
+
+public class ArrayDemo {
+
+  @SuppressWarnings("java:S106")
+  public static void main(String[] args) {
+    String[] colors = {"Red", "Green", "Blue", "Yellow"};
+
+    System.out.println("Contents of the array:");
+    System.out.println(colors[0]);
+    System.out.println(colors[1]);
+    System.out.println(colors[2]);
+    System.out.println(colors[3]);
+
+    System.out.println();
+
+    System.out.println("Length of the array: " + colors.length);
+
+    System.out.println();
+  }
+}
